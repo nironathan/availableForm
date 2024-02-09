b8a2ddb7893ff30137ab32db0e5c41fd139c30b0
nironathan <159393908+nironathan@users.noreply.github.com>
2024-02-09 01:38:30 +0000

    Create picker.js

---
+++ picker.js
@@ -0,0 +1,45 @@
+ <!-- // FORM WIZZARD START -->
+  
+<link rel="stylesheet" href="https://fengyuanchen.github.io/datepicker/css/datepicker.css">
+
+
+
+<style>
+.form-button {
+	outline: none;
+}
+
+.text-field, .text-area {
+	z-index: 2;
+}
+
+.text-field:focus + .field-label,
+.field-label.active {
+	font-size: 12px;
+  top: -8px;
+  z-index: 3;
+  background-color: white;
+}
+
+.text-area:focus + .area-label,
+.area-label.active {
+	font-size: 12px;
+  top: -8px;
+  z-index: 3;
+  background-color: white;
+}
+
+.text-field-done.active{
+	width: 24px;
+  height: 24px;
+  opacity: 1;
+}
+
+.text-field.done,
+.text-area.done {
+  border-color: rgba(127, 88, 226, 0.5);
+}
+</style>
+
+ <!-- // FORM WIZZARD END -->
+
